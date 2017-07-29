54b965e9ca76e27daa100ec348d52324e6d2aa3d
桑島侑己 <yukiny0811@gmail.com>
2017-07-29 23:01:48 +0900

    Initial Commit

---
+++ SudokuPractice/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  SudokuPractice
+//
+//  Created by 桑島侑己 on 2017/07/29.
+//  Copyright © 2017年 桑島侑己. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
